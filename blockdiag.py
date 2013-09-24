83bd824a5f320b58be7fd7abf3d2cf8654d454d2
judah <judah-unmuth-yockey@uiowa.edu>
2013-09-24 11:14:08 -0500

    added hermitian generator matrix

--- blockdiag.py
+++ blockdiag.py
@@ -8,7 +8,7 @@ from itertools import product
 from matplotlib import pyplot as plt 
 
 def lincom(matrix):
-    n=matrix.shape[0]
+    n = matrix.shape[0]
     one = np.identity(n**2)
     X = np.zeros((n, n, n**2))
     for i, j in product(range(n), range(n)):
@@ -19,3 +19,23 @@ def lincom(matrix):
     for i in range(n**2):
         B[i,:] = np.einsum('ia, a', C, XF[i,:])
     return B
+
+def hermx(matlist):
+    tol = 1e-10
+    n = np.shape(matlist[0])[0]
+    matlistdag = np.array([np.transpose(np.conjugate(matlist[a])) for a in range(len(matlist))])
+    T = np.array([lincom(matlist[a]) for a in range(matlist.shape[0])])
+    Tdag = np.array([lincom(matlistdag[a]) for a in range(matlistdag.shape[0])])
+    S = np.sum((np.einsum('ia, aj', np.transpose(np.conjugate(T[a])), T[a]) + np.einsum('ia, aj', np.transpose(np.conjugate(Tdag[a])), Tdag[a])) for a in range(len(T)))
+    O = np.linalg.eig(S)
+    num = np.sum(np.abs(O[0]) < tol)
+    u = []
+    for i in range(len(O[0])):
+        if O[0][i] < tol:
+            u.append(O[1][:,i])
+        elif (np.abs(O[0]) > tol).all():
+            raise ValueError("All eigenvalues greater than tolerance.")
+    c = np.random.random((np.shape(u)[0]))
+    c = c / np.sqrt(np.einsum('a, a', c, c))
+    u = (np.sum(np.array(u)[a] * c[a] for a in range(len(c)))).reshape(n,n)
+    return 0.5 * (u + np.transpose(np.conjugate(u)))
