06b4e4ca8883d3450c92b3ebbb94618724601725
judah <judah-unmuth-yockey@uiowa.edu>
2013-11-21 12:15:31 -0600

    made it more readable

--- clebsch-gordan.py
+++ clebsch-gordan.py
@@ -15,8 +15,13 @@ def clebgor(j1, j2, j, m1, m2, m):
     """
     zmin = int(min([j1 - m1, j2 + m2]))
     J = j1 + j2 + j
-    return kroneker(m, m1 + m2) * np.sqrt(binom(2 * j1, J - 2 * j) * binom(2 * j2, J - 2 * j)/ \
-            (binom(J + 1, J - 2 * j) * binom(2 * j1, j1 - m1) * \
-            binom(2 * j2, j2 - m2) * binom(2 * j, j - m))) * \
-            np.sum([(-1)**z * binom(J - 2 * j, z) * \
-            binom(J - 2 * j2, j1 - m1 - z) * binom(J - 2 * j1, j2 + m2 - z) for z in range(zmin + 1)])
+    return (kroneker(m, m1 + m2) *
+                np.sqrt(binom(2 * j1, J - 2 * j) *
+                binom(2 * j2, J - 2 * j)/
+                (binom(J + 1, J - 2 * j) * 
+                binom(2 * j1, j1 - m1) * 
+                binom(2 * j2, j2 - m2) * 
+                binom(2 * j, j - m))) * 
+                np.sum([(-1)**z * binom(J - 2 * j, z) * 
+                binom(J - 2 * j2, j1 - m1 - z) * 
+                binom(J - 2 * j1, j2 + m2 - z) for z in range(zmin + 1)]))
