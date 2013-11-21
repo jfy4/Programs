1c9dd8a80699fa140710e3b314081a0ccb6f3cc8
judah <judah-unmuth-yockey@uiowa.edu>
2013-11-20 20:38:03 -0600

    added a docstring

--- clebsch-gordan.py
+++ clebsch-gordan.py
@@ -5,6 +5,14 @@ import numpy as np
 kroneker = lambda a, b: 1.0 if a == b else 0.0
 
 def clebgor(j1, j2, j, m1, m2, m):
+    """
+    Parameters:    j1, j2, j: the angular momenta input
+                   m1, m2, m: the z components of angular momenta input
+    Returns:       The numerical value of the Clebsch-Gordan coeffcient.
+    Remarks:       Note that in the sum none of the binomial coeffcients can have
+                   negative values.  Thus, zmin is there to make sure that the sums
+                   have a cut-off.
+    """
     zmin = int(min([j1 - m1, j2 + m2]))
     J = j1 + j2 + j
     return kroneker(m, m1 + m2) * np.sqrt(binom(2 * j1, J - 2 * j) * binom(2 * j2, J - 2 * j)/ \
